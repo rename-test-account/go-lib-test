149e374b93cb029b7d356ec61474cfb0b6622e31
Carsten Seeger <c.seeger@epages.com>
2019-08-07 08:30:03 +0200

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rename-test-account/go-lib-test
+
+go 1.12
---
+++ test.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+// Test does a test print
+func Test() {
+	fmt.Println("this is a test")
+}
